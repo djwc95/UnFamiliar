01324cce7f5fd3915f1c39b9503192a29d2b1010
Jacob Tutor <tutorjacob@hotmail.com>
2023-04-10 21:46:35 -0500

    Puzzle In Cave Completed

--- UnFamiliar/Assets/Scripts/SequencePuzzleV2.cs
+++ UnFamiliar/Assets/Scripts/SequencePuzzleV2.cs
@@ -6,13 +6,11 @@ using UnityEngine;
 
 public class SequencePuzzleV2 : MonoBehaviour
 {
-    public DestroyThis destroyThis;
+    public OpenGate openGate;
     private int buttonsPushed = 0;
 
     public List<int> correctSequence = new List<int>();
     public List<int> currentSequence = new();
-
-    public AudioClip[] chime;
 
     public GameObject switch1;
     public GameObject switch2;
@@ -27,19 +25,46 @@ public class SequencePuzzleV2 : MonoBehaviour
     public GameObject rune1;
     public GameObject rune2;
     public GameObject rune3;
+    private GameObject destroyRune1; //destroy each rune
+    private GameObject destroyRune2;
+    private GameObject destroyRune3;
+    private GameObject destroyRune4;
+    private GameObject destroyRune5;
+
     public GameObject particles;
+    private GameObject destroyParticles1;
+    private GameObject destroyParticles2;
+    private GameObject destroyParticles3;
+    private GameObject destroyParticles4;
+    private GameObject destroyParticles5;
+
+    public AudioClip failSound;
+    public AudioClip passSound;
+    public AudioSource audioSource;
+
+    private bool canWin = true;
+    private bool canInteract = true;
 
     [SerializeField] private Animator gate = null;
 
     void Update()
     {
+        if (!canInteract)
+        {
+            return;
+        }
         if (currentSequence.SequenceEqual(correctSequence)) //do the sequences match?
         {
-            gate.Play("OpenGate", 0, 0f);
+            PuzzleWon();
+            canWin= false;
+            canInteract= false;
+
         }
         else if (buttonsPushed >= 5) //reset puzzle after we pushed all buttons
         {
-            destroyThis.Destroy();
+            StartCoroutine(ResetRunes());
+            openGate.Shake();
+            audioSource.PlayOneShot(failSound);
             buttonsPushed = 0;
             currentSequence.Clear();
         }
@@ -52,28 +77,28 @@ public class SequencePuzzleV2 : MonoBehaviour
 
         if (buttonsPushed == 1)
         {
-            Instantiate(rune1, position1.position, position1.rotation);
-            Instantiate(particles, position1.position, position1.rotation);
+            destroyRune1 =  Instantiate(rune1, position1.position, position1.rotation);
+            destroyParticles1 = Instantiate(particles, position1.position, position1.rotation);
         }
         else if (buttonsPushed == 2)
         {
-            Instantiate(rune1, position2.position, position2.rotation);
-            Instantiate(particles, position2.position, position2.rotation);
+            destroyRune2 = Instantiate(rune1, position2.position, position2.rotation);
+            destroyParticles2 = Instantiate(particles, position2.position, position2.rotation);
         }
         else if (buttonsPushed == 3)
         {
-            Instantiate(rune1, position3.position, position3.rotation);
-            Instantiate(particles, position3.position, position3.rotation);
+            destroyRune3 = Instantiate(rune1, position3.position, position3.rotation);
+            destroyParticles3 = Instantiate(particles, position3.position, position3.rotation);
         }
         else if (buttonsPushed == 4)
         {
-            Instantiate(rune1, position4.position, position4.rotation);
-            Instantiate(particles, position4.position, position4.rotation);
+            destroyRune4 = Instantiate(rune1, position4.position, position4.rotation);
+            destroyParticles4 = Instantiate(particles, position4.position, position4.rotation);
         }
         else if (buttonsPushed == 5)
         {
-            Instantiate(rune1, position5.position, position5.rotation);
-            Instantiate(particles, position5.position, position5.rotation);
+            destroyRune5 = Instantiate(rune1, position5.position, position5.rotation);
+            destroyParticles5 = Instantiate(particles, position5.position, position5.rotation);
         }
     }
 
@@ -83,28 +108,28 @@ public class SequencePuzzleV2 : MonoBehaviour
         buttonsPushed++;
         if (buttonsPushed == 1)
         {
-            Instantiate(rune2, position1.position, position1.rotation);
-            Instantiate(particles, position1.position, position1.rotation);
+            destroyRune1 = Instantiate(rune2, position1.position, position1.rotation);
+            destroyParticles1 = Instantiate(particles, position1.position, position1.rotation);
         }
         else if (buttonsPushed == 2)
         {
-            Instantiate(rune2, position2.position, position2.rotation);
-            Instantiate(particles, position2.position, position2.rotation);
+            destroyRune2 = Instantiate(rune2, position2.position, position2.rotation);
+            destroyParticles2 = Instantiate(particles, position2.position, position2.rotation);
         }
         else if (buttonsPushed == 3)
         {
-            Instantiate(rune2, position3.position, position3.rotation);
-            Instantiate(particles, position3.position, position3.rotation);
+            destroyRune3 = Instantiate(rune2, position3.position, position3.rotation);
+            destroyParticles3 = Instantiate(particles, position3.position, position3.rotation);
         }
         else if (buttonsPushed == 4)
         {
-            Instantiate(rune2, position4.position, position4.rotation);
-            Instantiate(particles, position4.position, position4.rotation);
+           destroyRune4 = Instantiate(rune2, position4.position, position4.rotation);
+           destroyParticles4 = Instantiate(particles, position4.position, position4.rotation);
         }
         else if (buttonsPushed == 5)
         {
-            Instantiate(rune2, position5.position, position5.rotation);
-            Instantiate(particles, position5.position, position5.rotation);
+            destroyRune5 = Instantiate(rune2, position5.position, position5.rotation);
+            destroyParticles5 = Instantiate(particles, position5.position, position5.rotation);
         }
     }
 
@@ -115,28 +140,55 @@ public class SequencePuzzleV2 : MonoBehaviour
 
         if (buttonsPushed == 1)
         {
-            Instantiate(rune3, position1.position, position1.rotation);
-            Instantiate(particles, position1.position, position1.rotation);
+            destroyRune1 = Instantiate(rune3, position1.position, position1.rotation);
+            destroyParticles1 = Instantiate(particles, position1.position, position1.rotation);
         }
         else if (buttonsPushed == 2)
         {
-            Instantiate(rune3, position2.position, position2.rotation);
-            Instantiate(particles, position2.position, position2.rotation);
+            destroyRune2 = Instantiate(rune3, position2.position, position2.rotation);
+            destroyParticles2 = Instantiate(particles, position2.position, position2.rotation);
         }
         else if (buttonsPushed == 3)
         {
-            Instantiate(rune3, position3.position, position3.rotation);
-            Instantiate(particles, position3.position, position3.rotation);
+            destroyRune3 = Instantiate(rune3, position3.position, position3.rotation);
+            destroyParticles3 = Instantiate(particles, position3.position, position3.rotation);
         }
         else if (buttonsPushed == 4)
         {
-            Instantiate(rune3, position4.position, position4.rotation);
-            Instantiate(particles, position4.position, position4.rotation);
+            destroyRune4 = Instantiate(rune3, position4.position, position4.rotation);
+            destroyParticles4 = Instantiate(particles, position4.position, position4.rotation);
         }
         else if (buttonsPushed == 5)
         {
-            Instantiate(rune3, position5.position, position5.rotation);
-            Instantiate(particles, position5.position, position5.rotation);
+            destroyRune5 = Instantiate(rune3, position5.position, position5.rotation);
+            destroyParticles5 = Instantiate(particles, position5.position, position5.rotation);
+        }
+    }
+
+    public IEnumerator ResetRunes()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(destroyRune1);
+        Destroy(destroyRune2);
+        Destroy(destroyRune3);
+        Destroy(destroyRune4);
+        Destroy(destroyRune5);
+        Destroy(destroyParticles1);
+        Destroy(destroyParticles2);
+        Destroy(destroyParticles3);
+        Destroy(destroyParticles4);
+        Destroy(destroyParticles5);
+
+
+    }
+
+    public void PuzzleWon()
+    {
+        if (canWin)
+        {
+            gate.Play("OpenGate", 0, 0f);
+            openGate.Shake();
+            audioSource.PlayOneShot(passSound);
         }
     }
 }
