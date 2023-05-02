260ca412f2304f8a585e279c11339accc686395e
Evan Mills <mrevanmills93@gmail.com>
2023-05-01 22:49:30 -0500

    majority of sounds in Park complete

---
+++ UnFamiliar/Assets/Scripts/seesawCreek.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seesawCreek : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public void Creek()
+    {
+        audioSource.Play();
+    }
+}
